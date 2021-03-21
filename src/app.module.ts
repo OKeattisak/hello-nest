831dcf9982e27c48cc2587faeb9623c137f6c059
OKeattisak <OKeattisak.A@outlook.com>
2021-03-21 23:08:04 +0700

    CRUD Generator - Users, Customers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CustomersModule } from './customers/customers.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, CustomersModule],
   controllers: [AppController],
   providers: [AppService],
 })
