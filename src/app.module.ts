3be16dc7f602e1761b483d494e2e5e3d2f2f127e
OKeattisak <OKeattisak.A@outlook.com>
2021-03-21 23:11:37 +0700

    CRUD Articles

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CustomersModule } from './customers/customers.module';
+import { ArticlesModule } from './articles/articles.module';
 
 @Module({
-  imports: [UsersModule, CustomersModule],
+  imports: [UsersModule, CustomersModule, ArticlesModule],
   controllers: [AppController],
   providers: [AppService],
 })
